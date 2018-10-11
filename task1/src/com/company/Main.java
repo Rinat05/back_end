b358ab0a16c389167ff068684ab76c444c150fa9
Rinat05 <Rinatbasik@gmail.com>
2018-10-11 23:07:37 +0300

    При помощи StringBuilder собрать любую строку и вывести

---
+++ task1/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("7bits");
+
+        sb.append(" is").append(" Nice!");
+
+        System.out.println(sb.toString());
+
+    }
+}
